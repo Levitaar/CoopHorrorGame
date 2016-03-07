68631646451f38a8c9178818aca2f955337d6ab3
Li Yang <yangl395@newschool.edu>
2016-03-07 18:25:58 -0500

    Networking works

---
+++ CameraGame/Assets/Assets/sender.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sender : MonoBehaviour {
+
+	// Use this for initialization
+	private Network networkhub;
+	private Vector3 xy;
+	void Start () {
+		networkhub=GameObject.Find ("nethub").GetComponent<Network> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		networkhub.NetworkSend (transform.position.x, transform.position.z);
+	}
+}
