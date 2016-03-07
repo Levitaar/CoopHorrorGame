ba794f8ef99b2eedf6e003e6dd910b2b35db32c1
Myrto Diakaki <diakv115@newschool.edu>
2016-03-07 14:33:32 -0500

    Player

--- 3spooky5me/Assets/PlayerMovement.cs
+++ 3spooky5me/Assets/PlayerMovement.cs
@@ -5,40 +5,36 @@ public class PlayerMovement : MonoBehaviour {
 
 	public float turnSmoothing = 15f;
 	public float speedDampTime = 0.1f;
-	public float speed;
 
 	private Animator anim;
 	private HashIDs hash;
 
-	void Awake(){
+	void setup(){
+		
 		anim = GetComponent<Animator> ();
-		hash = GameOnject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+		hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+
 	}
-
-
+		
 	void FixedUpdate(){
+		
 		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
 		bool sneak = Input.GetButton ("Sneak");
 
-		MovementManagement (h, v, sneak);
+		MovementManagment (h, v, sneak);
 	}
 
 	void MovementManagment(float horizontal, float vertical, bool sneaking){ 
+
 		anim.SetBool (hash.sneakingBool, sneaking);
-		if (horizontal != 0f || vertical != 0f) {
-			Rotating(horizontal, vertical);
-			SetFloat (speed = 5.5f, speedDampTime, Time.deltaTime);
+
+		if (horizontal != 0f || vertical != 0f){
+			anim.SetFloat (hash.speedFloat, 5.5f, speedDampTime, Time.deltaTime);
 				
 		} else {
-			speed = 0f
+			anim.SetFloat (hash.speedFloat, 0f);
 		}
 
 	}
-		
-	void Rotating(float horizontal, float vertical){
-		Vector3 tragetDirection = new Vector3 (horizontal, 0f, vertical);
-		Quaternion targetRotation = Quaternion.LookRotation (targetDirection, Vector3.up);
-		Quaternion newRotation = Quaternion.Lerp (rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
-		rigidbody.MoveRotation (newRotation);
-	}
+}
