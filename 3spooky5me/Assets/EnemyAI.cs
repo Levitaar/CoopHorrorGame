a125c30068f557e6510bb93913bef9532822ab06
Myrto Diakaki <diakv115@newschool.edu>
2016-03-07 17:30:22 -0500

    no message

---
+++ 3spooky5me/Assets/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+	public int maxdistance;
+
+	private Transform myTransform;
+
+	void Awake(){
+		myTransform = transform;
+	}
+
+
+	void Start () {
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+
+		target = go.transform;
+
+		maxdistance = 2;
+	}
+
+
+	void Update () { 
+		
+
+		if(Vector3.Distance(target.position, myTransform.position) > maxdistance){
+			
+			myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+		}
+	}
+}
