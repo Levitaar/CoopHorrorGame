20ead72507cc00b7166c907ec84681775a415724
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-03-07 17:30:56 -0500

    Moving to Folder: CameraGame

---
+++ CameraGame/Assets/Assets/HashIDs.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+
+	public int dyingState;
+	public int locomotionState;
+	public int deadBool;
+	public int speedFloat;
+	public int sneakingBool;
+	public int playerInSightBool;
+
+
+	void setup ()
+	{
+		dyingState = Animator.StringToHash("Base Layer.Dying");
+		locomotionState = Animator.StringToHash("Base Layer.Locomotion");
+		deadBool = Animator.StringToHash("Dead");
+		speedFloat = Animator.StringToHash("Speed");
+		sneakingBool = Animator.StringToHash("Sneaking");
+		playerInSightBool = Animator.StringToHash("PlayerInSight");
+	}
+}
---
+++ CameraGame/Assets/Assets/PrefabSelector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PrefabSelector : NetworkManager {
+
+	public override void OnServerConnect(NetworkConnection conn) {
+
+		print ("player connected");
+
+		if (Network.isClient) {
+
+			print ("Player is Client");
+
+		} else if (Network.isServer) {
+
+			print ("Player is Server");
+
+		}
+
+	}
+
+}
