26e8029b2339684f6c5f28f4ac2810c9cdb0a1e6
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-03-07 17:59:31 -0500

    no message

---
+++ CameraGame/Assets/Assets/Network.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Application;
+
+public class Network : MonoBehaviour {
+	void Start () {}
+
+	private const string URL = "http://california.thixlab.com:9000/xy";
+	private Vector2 xy;
+	private bool loaded;
+	public void NetworkLoad(){
+		loaded = false;
+		StartCoroutine(Load(new WWW(URL)));
+	}
+	public void NetworkSend(float x,float y){
+		StartCoroutine(Request(new WWW(URL+"?x="+x+"&y="+y)));
+	}
+
+	public Vector2 getXY(){
+		return xy;
+	}
+	public bool isLoaded(){
+		return loaded;
+	}
+
+	private IEnumerator Load(WWW www)
+	{
+		yield return www;
+		// check for errors
+		if (www.error == null)
+		{
+			loaded = true;
+
+			xyObject myXY = JsonUtility.FromJson<xyObject>(www.text);
+			xy = new Vector2 (float.Parse(myXY.x), float.Parse(myXY.y));
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+
+	private IEnumerator Request(WWW www)
+	{
+		yield return www;
+		// check for errors
+		if (www.error == null)
+		{
+			//Debug.Log(www.text);
+		} else {
+			Debug.Log("WWW Error: "+ www.error);
+		}    
+	}
+
+}
---
+++ CameraGame/Assets/Assets/Reciever.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	private Network networkhub;
+	void Start () {
+		networkhub=GameObject.Find ("network hub").GetComponent<Network> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (networkhub.isLoaded ()) {
+			Debug.Log (networkhub.getXY ());
+		} else {
+			networkhub.NetworkLoad ();
+		}
+
+		networkhub.NetworkSend (Input.mousePosition.x, Input.mousePosition.y);
+	}
+}
