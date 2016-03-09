7aa0f363bfeac81cbb887e86f44b1e007639c77f
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-03-09 15:57:13 -0500

    no message

--- 3spooky5me/Assets/Tags.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tags : MonoBehaviour
-{
-	public const string player = "Player";
-	public const string gameController = "GameController";
-	public const string enemy = "Enemy";
-}
