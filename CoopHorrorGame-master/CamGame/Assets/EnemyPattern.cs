d6d2e80bdba9cd1d187804f707e393c016066f7f
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-03-15 18:24:01 -0400

    Finished ENEMY AI! (HYPED)

--- CoopHorrorGame-master/CamGame/Assets/EnemyPattern.cs
+++ CoopHorrorGame-master/CamGame/Assets/EnemyPattern.cs
@@ -3,59 +3,71 @@ using System.Collections;
 
 public class EnemyPattern : MonoBehaviour {
 
+	public bool ToA;
 	public bool IsPlayerDead;
 	public bool InSight;
-	public float patrolSpeed = 3f;
+	public bool lerping;
+
+	public float patrolSpeed;
 	public float chaseSpeed = 5f;
-	public bool direction;
+
 	public GameObject WaypointA;
 	public GameObject WaypointB;
-	public Transform player;
+	public GameObject player;
+	private Vector3 startpoint;
+	private Vector3 endpoint;
 
+	private int direction;
 
-	void Setup() {
+	void Start() {
 
+		startpoint = WaypointA.transform.position;
+		endpoint = WaypointB.transform.position;
 		InSight = false;
-		gameObject.transform.position = WaypointA.transform.position;
+		transform.position = startpoint;
 
 	}
 
 	void Update() {
 
-		if (InSight == true) {
 
+		if (InSight == true) {
+			lerping = false;
+
+			print ("I SEE DEAD PEOPLE");
+
+			transform.position = Vector3.MoveTowards (transform.position, player.transform.position, Time.deltaTime * chaseSpeed);
+			transform.LookAt (player.transform.position);
 
 		} 
 
 		if (InSight == false) {
 
-			if (gameObject.transform.position == WaypointA.transform.position) {
+			print ("CANT SEE");
 
-				direction = true;
-
-			}
-
-			if (gameObject.transform.position == WaypointB.transform.position) {
-
-				direction = false;
+			if (transform.position == startpoint) {
+				print ("TO END POINT");
+				ToA = true;
 
 			}
 
-			if (direction == true) {
-
-				transform.LookAt (WaypointB);
-				transform.position += Vector3.forward * Time.deltaTime * patrolSpeed;
+			if (transform.position == endpoint) {
+				print ("TO START POINT");
+				ToA = false;
 
 			}
 
-			if (direction == false) {
+			if (ToA) {
+				transform.position = Vector3.MoveTowards (transform.position, endpoint, Time.deltaTime * patrolSpeed);
+				transform.LookAt (WaypointB.transform.position);
 
-				transform.LookAt (WaypointA);
-				transform.position += Vector3.forward * Time.deltaTime * patrolSpeed;
+			} else {
+				transform.position = Vector3.MoveTowards (transform.position, startpoint, Time.deltaTime * patrolSpeed);
+				transform.LookAt (WaypointA.transform.position);
+
 			}
-
 		}
-	}
+	} 
 
 	void OnTriggerStay(Collider other) {
 
